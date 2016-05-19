1215437173964bdcf12967776dc724d6bb0c22dd
Matúš Petrofčík <petrofcikmatus@gmail.com>
2016-05-20 01:36:31 +0200

    update

--- _app/layouts/header.php
+++ _app/layouts/header.php
@@ -6,3 +6,11 @@
     <link rel="stylesheet" href="<?= url("assets/css/app.css") ?>">
 </head>
 <body>
+
+<?php if (has_messages()) : ?>
+    <div class="messages">
+        <?php foreach (get_messages() as $message) : ?>
+            <div class="message"><?= plain($message) ?></div>
+        <?php endforeach ?>
+    </div>
+<?php endif ?>
