9251285b836fffe9769938d86ca92da6c5dece0f
Matúš Petrofčík <petrofcikmatus@gmail.com>
2016-05-29 01:44:03 +0200

    update

--- _app/pages/error/error-404.php
+++ _app/pages/error/error-404.php
@@ -3,8 +3,8 @@
     <div class="container">
         <div class="jumbotron">
             <h1>A jeje! Chyba 404 :'(</h1>
-            <h2>To čo hľadáš nenájdeš! Takáto stránka totiž zeexistuje.</h2>
-            <p>Pravdepodobne máš preklep v url adrese alebo ste klikli na nefunkčný odkaz.</p>
+            <h2>To čo hľadáš nenájdeš! Takáto stránka totiž neexistuje.</h2>
+            <p>Pravdepodobne máš preklep v url adrese alebo si klikol na nefunkčný odkaz.</p>
             <p><a class="btn btn-primary" href="<?= url() ?>">Úvodná stránka</a></p>
         </div>
     </div>
